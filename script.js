259b0d47c43bc229f21b58600850755440029695
chinmaya <chinmayackr@gmail.com>
2025-09-25 00:32:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll and basic interactions
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    e.preventDefault();
+    const target = document.querySelector(a.getAttribute('href'));
+    if(target) target.scrollIntoView({behavior:'smooth', block:'start'});
+  });
+});
